e74ce332d21dd9a7157b2c71f09c250407277043
David Pinilla <contact@davidpinilla.com>
2020-10-24 11:05:58 +0200

    add total supply token test

--- test/DavidToken.test.js
+++ test/DavidToken.test.js
@@ -7,11 +7,11 @@ contract('DavidToken',accounts =>{
     let davidToken;
     const _name = 'DavidToken';
     const _symbol = 'DAVID';
-
+    const _totalSupply = 21000000;
     beforeEach(async ()=>{
          davidToken = await DavidToken.new()
     });
-    
+
     describe('Token attributes',() => {
         it('has the correct name',async () => {
            const name = await davidToken.name();
@@ -21,5 +21,9 @@ contract('DavidToken',accounts =>{
             const symbol = await davidToken.symbol();
             symbol.should.equal(_symbol);
         });
+        it('has a total supply of '+_totalSupply, async ()=>{
+            const totalSupply = await davidToken.totalSupply();
+            totalSupply.toNumber().should.equal(_totalSupply);
+        })
     })
 })
